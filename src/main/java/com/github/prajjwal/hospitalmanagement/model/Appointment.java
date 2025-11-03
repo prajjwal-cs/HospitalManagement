f6e6f4833767b1123e671cd65d28911d09060394
prajjwal <prajjwalpachauri725@gmail.com>
2025-11-03 17:52:04 +0530

    created and implemented appointment

---
+++ src/main/java/com/github/prajjwal/hospitalmanagement/model/Appointment.java
@@ -0,0 +1,37 @@
+package com.github.prajjwal.hospitalmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentTime;
+
+    @Column(length = 500)
+    private String reason;
+
+    @ManyToOne
+    @ToString.Exclude
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    @ToString.Exclude
+    private Doctor doctor;
+}
