a402649188090eb6b82d8d620818dc3292a301bc
prajjwal <prajjwalpachauri725@gmail.com>
2025-11-03 17:51:31 +0530

    created and implemented doctor

---
+++ src/main/java/com/github/prajjwal/hospitalmanagement/model/Doctor.java
@@ -0,0 +1,40 @@
+package com.github.prajjwal.hospitalmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne
+    @MapsId
+    private User user;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 100)
+    private String specialization;
+
+    @Column(unique = true, length = 100)
+    private String email;
+
+
+    @ManyToMany(mappedBy = "doctors")
+    private Set<Department> departments = new HashSet<>();
+
+    @OneToMany(mappedBy = "doctor")
+    private List<Appointment> appointments = new ArrayList<>();
+}
